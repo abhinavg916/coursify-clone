4e515b39029bcfd698455bab1ed355f8df345ae6
Abhi <Abhinavg916@gmail.com>
2020-12-04 01:14:28 +0530

    Smooth Scrolling

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+$("body").scrollspy({ target: "#main-nav" });
+
+$("#main-nav a").on("click", function (e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      1000,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
