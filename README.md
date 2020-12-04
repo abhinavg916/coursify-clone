a30e2eb853f158c01ae36239b0ab2f0d06fa8677
Abhi <Abhinavg916@gmail.com>
2020-12-04 18:36:19 +0530

    Animations

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Tripupp Assignment
+
 [![Netlify Status](https://api.netlify.com/api/v1/badges/b20da72d-979b-4b54-95e1-8e4f89edd57f/deploy-status)](https://app.netlify.com/sites/clever-borg-6320b0/deploys)
 
 > Tip: Make sure to have good a internet connection
@@ -7,6 +8,7 @@
 - Visit - [Coursify](https://clever-borg-6320b0.netlify.app/)
 
 ## Tech Used
+
 - HTML
 - CSS
 - Bootstrap 4
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,116 @@
-$("body").scrollspy({ target: "#main-nav" });
+// $("body").scrollspy({ target: "#main-nav" });
 
-$("#main-nav a").on("click", function (e) {
-  if (this.hash !== "") {
-    e.preventDefault();
+// $("#main-nav a").on("click", function (e) {
+//   if (this.hash !== "") {
+//     e.preventDefault();
 
-    const hash = this.hash;
+//     const hash = this.hash;
 
-    $("html, body").animate(
-      {
-        scrollTop: $(hash).offset().top,
-      },
-      1000,
-      function () {
-        window.location.hash = hash;
+//     $("html, body").animate(
+//       {
+//         scrollTop: $(hash).offset().top,
+//       },
+//       1000,
+//       function () {
+//         window.location.hash = hash;
+//       }
+//     );
+//   }
+// });
+
+// Smooth Scrolling
+$(function () {
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000
+        );
+        return false;
       }
-    );
-  }
+    }
+  });
+});
+
+// Animation
+window.sr = ScrollReveal();
+
+sr.reveal(".navbar", {
+  duration: 1500,
+  origin: "bottom",
+});
+
+sr.reveal(".showcase-top", {
+  duration: 1500,
+  origin: "top",
+  distance: "300px",
+});
+
+sr.reveal(".showcase-right", {
+  duration: 1500,
+  origin: "right",
+  distance: "300px",
+});
+
+sr.reveal(".showcase-bottom", {
+  duration: 1500,
+  origin: "bottom",
+  distance: "300px",
+});
+
+sr.reveal(".showcase-left", {
+  duration: 1500,
+  origin: "left",
+  distance: "300px",
+});
+
+sr.reveal(".showcase-simple", {
+  duration: 1500,
+  origin: "bottom",
+});
+
+sr.reveal(".showcase-btn", {
+  duration: 1500,
+  delay: 1500,
+  origin: "bottom",
+});
+
+sr.reveal(".info-top", {
+  duration: 1500,
+  origin: "top",
+  distance: "300px",
+  viewFactor: 0.2,
+});
+
+sr.reveal(".info-right", {
+  duration: 1500,
+  origin: "right",
+  distance: "300px",
+  viewFactor: 0.2,
+});
+
+sr.reveal(".info-bottom", {
+  duration: 1500,
+  origin: "bottom",
+  distance: "300px",
+  viewFactor: 0.2,
+});
+
+sr.reveal(".info-left", {
+  duration: 1500,
+  origin: "left",
+  distance: "300px",
+  viewFactor: 0.2,
+});
+
+sr.reveal(".info-simple", {
+  duration: 1500,
+  origin: "bottom",
+  distance: "300px",
+  viewFactor: 0.2,
 });
